cb27ea5b226983507a1074aabb467a8c61849c9c
Austin Burgard <awburgard@gmail.com>
2019-05-20 13:24:52 -0500

    adds speedReducer and ability to increment on page

--- src/components/SpeedControl/SpeedControl.js
+++ src/components/SpeedControl/SpeedControl.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import mapReduxStateToProps from '../../Modules/mapReduxStateToProps'
 // THIS COMPONENT IS OUR INTERFACE FOR SPEED
 // YOU SHOULD DISPLAY THE CURRENT SPEED
 // BUTTONS SHOULD INCREASE OR DECREASE SPEED, RESPECTIVELY
 
 class SpeedControl extends Component {
   render() {
+
+    const increaseSpeed = (event) => {
+      this.props.dispatch({
+        type: 'INCREASE_SPEED'
+      })
+    }
+
+    const decreaseSpeed = (event) => {
+      this.props.dispatch({
+        type: 'DECREASE_SPEED'
+      })
+    }
+
     return (
       <div>
         <h2>Speed Control</h2>
 
-        <button>Increase Speed</button>
-        <p>SPEED: GOES HERE</p>
-        <button>Decrease Speed</button>
+        <button onClick={increaseSpeed}>Increase Speed</button>
+        <p>SPEED: {this.props.reduxState.speedReducer}</p>
+        <button onClick={decreaseSpeed}>Decrease Speed</button>
       </div>
     )
   }
 }
 
-export default SpeedControl;
+export default connect(mapReduxStateToProps)(SpeedControl);
